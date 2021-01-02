305ba912266f6e94ee7f3bc841d6b76b38cfbba2
KAB <>
2021-01-02 10:22:48 +0000

    Set up factory bot

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :post do
-    caption { "MyString" }
+    caption { "#nofilter" }
+    image Rack::Test::UploadedFile.new(Rails.root +
+    'spec/files/images/coffee.jpg', 'image/jpg')
   end
 end
